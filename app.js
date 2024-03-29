aab69da74e689e31004054113f679e3fec590bf9
sujalg436 <sujalghorse9@outlook.com>
2024-03-29 16:40:37 +0530

    array added in js file

---
+++ app.js
@@ -0,0 +1,16 @@
+const questions = [
+    {
+        question: "what is your name?",
+        choice:["gelcho", "madcho", "bencho"],
+        correct: 2
+    },
+    {
+        question: "what is your age?",
+        choice:["40", "20", "10"],
+        correct: 1
+    },   {
+        question: "what is your size?",
+        choice:["10px", "20px", "30px"],
+        correct: 0
+    },
+];
